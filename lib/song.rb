990a8b0b63733bedd52eadc3784ff68095d579a7
wolfene2001 <wolfene2001@yahoo.com>
2020-01-01 15:33:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song
 
+  attr_accessor :name, :artist, :genre
+  
   @@count = 0
   @@genres = []
   @@artists = []
-
-  attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name

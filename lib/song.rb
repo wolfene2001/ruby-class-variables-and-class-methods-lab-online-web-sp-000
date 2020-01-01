70122a5fc22c8d84ea3ab4f72198b7fe32ea983d
wolfene2001 <wolfene2001@yahoo.com>
2020-01-01 15:27:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,4 +26,18 @@ class Song
   def self.artists
     @@artists.uniq!
   end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  
 end

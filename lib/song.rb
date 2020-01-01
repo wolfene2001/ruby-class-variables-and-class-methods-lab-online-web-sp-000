3d3eaf2b190364f9abe8c729ed89ad20c6fc3494
wolfene2001 <wolfene2001@yahoo.com>
2020-01-01 15:33:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,8 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres = genre
-    @@artists = artist
+    @@genres << genre
+    @@artists << artist
   end
 
   def self.count

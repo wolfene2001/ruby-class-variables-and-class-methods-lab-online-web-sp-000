dd33c06e9ce6baa637271edaefbe3ff120b80391
wolfene2001 <wolfene2001@yahoo.com>
2020-01-01 14:52:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,12 +4,7 @@ class Song
   @@genres = []
   @@artists = []
 
-
-  def self.count
-    @@count
-  end
-
-attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
@@ -18,5 +13,17 @@ attr_accessor :name, :artist, :genre
     @@count += 1
     @@genres = genre
     @@artists = artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.artists
+    @@artists.uniq!
   end
 end
